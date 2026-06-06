e28d8b45849e22e24217794c35674925e85af390
Phukhao04 <6610210312@psu.ac.th>
2026-06-06 20:14:09 +0700

    fix basename

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,18 +2,17 @@ import { BrowserRouter } from 'react-router-dom';
 
 import ContextProvider from './contexts/ContextProvider';
 import Layout from './components/templates/Layout';
+import './App.css';
 const isProduction = import.meta.env.MODE === 'production';
 const { VITE_basename } = import.meta.env;
-import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename={isProduction ? VITE_basename : '/'}>
+    <BrowserRouter basename="/">
       <ContextProvider>
         <Layout />
       </ContextProvider>
     </BrowserRouter>
   );
 }
-
 export default App;
